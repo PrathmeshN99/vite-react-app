47785409164a2363013f58274ab6e7802661077f
Prathmesh-Nagpure-OPSNETIC <prathmesh.nagpure@opsnetic.com>
2024-04-28 15:27:29 +0530

    Added sso

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import "./App.css";
 import BasicTable from "./components/BasicTable";
+import "./App.css";
+import LoginSignup from "./components/LoginSignup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <h1>React Table</h1>
-      <BasicTable />
-    </>
+    <Router>
+      {/* <Header siteTitle="SLACK SSO" /> */}
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<BasicTable />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
+
+const Home = () => {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+};
+
 
 export default App;
--- src/components/BasicTable.tsx
+++ src/components/BasicTable.tsx
@@ -1,14 +1,12 @@
+import React, { useState, useEffect, useMemo } from "react";
 import {
-  useReactTable,
-  getCoreRowModel,
   flexRender,
-  TableOptions,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
 } from "@tanstack/react-table";
-import { useMemo, useEffect, useState } from "react";
-
-interface HashtagsObject {
-  [key: string]: number;
-}
 
 interface DataItem {
   _id: string;
@@ -17,32 +15,25 @@ interface DataItem {
   starsReceived: number;
   starsLeft: number;
   text: string;
-  hashtags: HashtagsObject;
+  hashtags: { [key: string]: number };
   channels: string[];
 }
 
-const BasicTable = () => {
+const BasicTable: React.FC = () => {
   const [data, setData] = useState<DataItem[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [sorting, setSorting] = useState<any[]>([]);
+  const [filtering, setFiltering] = useState<string>("");
 
   useEffect(() => {
-    console.log("Started");
     const fetchDataFromServer = async () => {
       try {
-        console.log("Fetching data from server...");
-        setIsLoading(true);
-
         const response = await fetch(
           "https://leaderboard-server.vercel.app/api/data"
         );
         const data: DataItem[] = await response.json();
-
         setData(data);
-        console.log("Data fetched from server:", data);
       } catch (error) {
         console.error("Error fetching data from server:", error);
-      } finally {
-        setIsLoading(false);
       }
     };
 
@@ -51,83 +42,125 @@ const BasicTable = () => {
 
   const columns = useMemo(
     () => [
-      {
-        header: "ID",
-        accessorKey: "_id",
-      },
-      {
-        header: "User",
-        accessorKey: "user",
-      },
-      {
-        header: "Name",
-        accessorKey: "name",
-      },
-      {
-        header: "Stars Received",
-        accessorKey: "starsReceived",
-      },
-      {
-        header: "Stars Left",
-        accessorKey: "starsLeft",
-      },
-      {
-        header: "Text",
-        accessorKey: "text",
-      },
+      { header: "ID", accessorKey: "_id" },
+      { header: "User", accessorKey: "user" },
+      { header: "Name", accessorKey: "name" },
+      { header: "Stars Received", accessorKey: "starsReceived" },
+      { header: "Stars Left", accessorKey: "starsLeft" },
+      { header: "Text", accessorKey: "text" },
       {
         header: "Hashtags",
         accessorKey: "hashtags",
+        cell: ({ getValue }: { getValue: () => { [key: string]: number } }) => {
+          const hashtags = getValue();
+          if (!hashtags) return null;
+
+          return (
+            <div>
+              {Object.entries(hashtags).map(([key, value]) => (
+                <div key={key}>{`${key}: ${value}`}</div>
+              ))}
+            </div>
+          );
+        },
       },
-      {
-        header: "Channels",
-        accessorKey: "channels",
-      },
+      { header: "Channels", accessorKey: "channels" },
     ],
     []
   );
 
-  const options: TableOptions<DataItem> = {
+  const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
-  };
-
-  const table = useReactTable(options);
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting,
+      globalFilter: filtering,
+    },
+    onSortingChange: setSorting,
+    onGlobalFilterChange: setFiltering,
+  });
 
   return (
-    <div>
-      {isLoading ? (
-        <p>Loading data from server...</p>
-      ) : (
-        <table>
-          <thead>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <th key={header.id}>
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody>
-            {table.getRowModel().rows.map((row) => (
-              <tr key={row.id}>
-                {row.getVisibleCells().map((cell) => (
-                  <td key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+    <div className="min-h-screen bg-gray-900 text-white font-sans">
+      <input
+        type="text"
+        value={filtering}
+        onChange={(e) => setFiltering(e.target.value)}
+        className="bg-gray-800 text-white px-4 py-2 rounded-md border-none focus:outline-none"
+      />
+      <table className="w-full table-auto">
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th
+                  key={header.id}
+                  onClick={header.column.getToggleSortingHandler()}
+                  className="px-4 py-2 text-left cursor-pointer"
+                >
+                  {header.isPlaceholder ? null : (
+                    <div className="flex items-center">
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                      {header.column.getIsSorted()
+                        ? header.column.getIsSorted() === "asc"
+                          ? "🔼"
+                          : "🔽"
+                        : null}
+                    </div>
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id} className="px-4 py-2">
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="flex justify-center space-x-4 mt-4">
+        <button
+          onClick={() => table.setPageIndex(0)}
+          className="px-4 py-2 rounded-md bg-blue-500 text-white focus:outline-none hover:bg-blue-600"
+        >
+          First page
+        </button>
+        <button
+          disabled={!table.getCanPreviousPage()}
+          onClick={() => table.previousPage()}
+          className="px-4 py-2 rounded-md bg-blue-500 text-white focus:outline-none hover:bg-blue-600"
+        >
+          Previous page
+        </button>
+        <button
+          disabled={!table.getCanNextPage()}
+          onClick={() => table.nextPage()}
+          className="px-4 py-2 rounded-md bg-blue-500 text-white focus:outline-none hover:bg-blue-600"
+        >
+          Next page
+        </button>
+        <button
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          className="px-4 py-2 rounded-md bg-blue-500 text-white focus:outline-none hover:bg-blue-600"
+        >
+          Last page
+        </button>
+      </div>
     </div>
   );
 };
